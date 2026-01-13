648d1fdeeeaf3b87a9731f86c8b7452a41ef62fe
Ronell Salunke <ronell1292k@gmail.com>
2026-01-13 11:04:20 +0530

    fix: delete Flutter splash screen, causes unnecessary delay
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- lib/app/routes/router.dart
+++ lib/app/routes/router.dart
@@ -1,13 +1,11 @@
-import 'package:go_router/go_router.dart';
-import 'package:bs_flutter/app/modules/splash/splash_screen.dart';
+import 'package:bs_flutter/app/modules/edit_bill/edit_bill_screen.dart';
 import 'package:bs_flutter/app/modules/home/home_screen.dart';
 import 'package:bs_flutter/app/modules/settings/settings_screen.dart';
-import 'package:bs_flutter/app/modules/edit_bill/edit_bill_screen.dart';
+import 'package:go_router/go_router.dart';
 
 final GoRouter router = GoRouter(
   routes: [
-    GoRoute(name: 'splash', path: '/', builder: (context, state) => const SplashScreen()),
-    GoRoute(name: 'home', path: '/home', builder: (context, state) => const HomeScreen()),
+    GoRoute(name: 'home', path: '/', builder: (context, state) => const HomeScreen()),
     GoRoute(name: 'settings', path: '/settings', builder: (context, state) => const SettingsScreen()),
     GoRoute(
       name: 'bill',
