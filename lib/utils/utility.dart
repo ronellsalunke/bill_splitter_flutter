888a574f0e371fcb1d16dcc25579881b4a12dc50
Ronell Salunke <ronell1292k@gmail.com>
2026-03-21 18:39:05 +0530

    fix(utils): handle multiple connectivity results
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- lib/utils/utility.dart
+++ lib/utils/utility.dart
@@ -7,7 +7,7 @@ import 'package:image_picker/image_picker.dart';
 class Utility {
   static Future<bool> hasInternetConnection() async {
     final result = await Connectivity().checkConnectivity();
-    return result != ConnectivityResult.none;
+    return result.any((status) => status != ConnectivityResult.none);
   }
 
   static Future<File?> pickImage(BuildContext context, ImageSource source) async {
