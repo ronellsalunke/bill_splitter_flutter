c9e09c00361fd568bac9a70fc992df7304700f00
Ronell Salunke <ronell1292k@gmail.com>
2026-07-24 14:28:18 +0530

    chore: remove unused/redundant extensions
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- lib/extensions/widget_extensions.dart
+++ lib/extensions/widget_extensions.dart
@@ -3,64 +3,18 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 extension WidgetExtension on Widget? {
-
-  /// return padding top
-  Padding paddingTop(double top) {
-    return Padding(
-      padding: EdgeInsets.only(top: top),
-      child: this,
-    );
-  }
-
-  /// return padding left
-  Padding paddingLeft(double left) {
-    return Padding(
-      padding: EdgeInsets.only(left: left),
-      child: this,
-    );
-  }
-
-  /// return padding right
-  Padding paddingRight(double right) {
-    return Padding(
-      padding: EdgeInsets.only(right: right),
-      child: this,
-    );
-  }
-
-  /// return padding bottom
-  Padding paddingBottom(double bottom) {
-    return Padding(
-      padding: EdgeInsets.only(bottom: bottom),
-      child: this,
-    );
-  }
-
-  /// return padding all
   Padding paddingAll(double padding) {
     return Padding(padding: EdgeInsets.all(padding), child: this);
   }
 
-  /// return custom padding from each side
   Padding paddingOnly({double top = 0.0, double left = 0.0, double bottom = 0.0, double right = 0.0}) {
     return Padding(padding: EdgeInsets.fromLTRB(left, top, right, bottom), child: this);
   }
 
-  /// return padding symmetric
   Padding paddingSymmetric({double vertical = 0.0, double horizontal = 0.0}) {
     return Padding(
       padding: EdgeInsets.symmetric(vertical: vertical, horizontal: horizontal),
       child: this,
-    );
-  }
-
-  /// Blur
-  Widget backgroundBlur({double? blur, Color? bgColor}) {
-    return ClipRRect(
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaY: blur ?? 20, sigmaX: blur ?? 20, tileMode: TileMode.mirror),
-        child: Container(color: bgColor ?? Colors.white30, child: this!),
-      ),
     );
   }
 }
