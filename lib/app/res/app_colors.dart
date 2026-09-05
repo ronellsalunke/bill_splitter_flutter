d4170a391b2c1dd0ca8e028eee6e3eaf9e5a5639
Ronell Salunke <ronell1292k@gmail.com>
2026-09-06 00:31:39 +0530

    feat: make dark mode bg easier on the eyes
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- lib/app/res/app_colors.dart
+++ lib/app/res/app_colors.dart
@@ -2,5 +2,5 @@ import 'dart:ui';
 
 class AppColors {
   static const Color backgroundColorLight = Color(0xFFE4E4E4);
-  static const Color backgroundColorDark = Color(0xFF0B0B0B);
+  static const Color backgroundColorDark = Color(0xFF121212);
 }
